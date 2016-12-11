51ca5fcd3f601e1927e14547836f5587fe71ddea
Margus Sellin <sellinmargus@gmail.com>
2016-12-11 20:22:58 +0200

    use jsoup instead

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -1,9 +1,30 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Crawler {
 
     public void doSearch(String url) throws Exception {
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.select("a[href]");
+        List<String> list = new ArrayList<String>();
+
+        for (Element link : links) {
+            String text = link.attr("abs:href");
+            System.out.println(text);
+            list.add(text);
+        }
+        /*
         String urls[] = new String[1000];
+
+
+
+
         int i=0,j=0,tmp=0,total=0, MAX = 1000;
         int start=0, end=0;
         String webpage = Web.getWeb(url);
@@ -37,7 +58,18 @@ public class Crawler {
             urls[i] = url;
             System.out.println(urls[i]);
         }
-        System.out.println("Total URLS Fetched are " + total);
+        */
+        System.out.println("Total URLS Fetched are " + list.size());
+    }
+
+    private String loadHtmlFromUrl(String url) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("http://example.com/").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return doc.outerHtml();
     }
 
     public static void main(String[] args) throws Exception{
